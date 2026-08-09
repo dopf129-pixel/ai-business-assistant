994365c59616889b48b5585522ef320b908c4b23
gelashvilee <dopf129@mail.ru>
2026-08-09 12:22:48 +0300

    Add assistant entry service flow

---
+++ app/services/assistant_entry_service.py
@@ -0,0 +1,31 @@
+class AssistantEntryService:
+
+
+    def __init__(
+        self,
+        main_flow_service
+    ):
+
+        self.main_flow_service = (
+            main_flow_service
+        )
+
+
+    def handle(
+        self,
+        text
+    ):
+
+        report = {
+            "sales_down": True,
+            "low_stock": True
+        }
+
+
+        return (
+            self.main_flow_service
+            .process(
+                text,
+                report
+            )
+        )
