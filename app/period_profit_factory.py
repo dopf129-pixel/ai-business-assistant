25a1760fa5996962b7fac64ff0247151805cbbf8
dopf129-pixel <dopf129@gmail.com>
2026-08-29 20:50:45 +0300

    Wire return evidence into period profit query

--- app/period_profit_factory.py
+++ app/period_profit_factory.py
@@ -1,7 +1,9 @@
+from api.ozon_client import OzonClient
 from config import TAX_RATE
 from services.cost_service import ProductCostService
 from services.finance_service import FinanceService
 from services.period_profit_query_service import PeriodProfitQueryService
+from services.period_profit_return_evidence_service import PeriodProfitReturnEvidenceService
 from services.period_profit_summary_service import PeriodProfitSummaryService
 from services.product_service import ProductService
 
@@ -16,4 +18,5 @@ def create_period_profit_query():
     return PeriodProfitQueryService(
         summary_service=summary_service,
         product_provider=product_service.load_products,
+        return_evidence_service=PeriodProfitReturnEvidenceService(OzonClient()),
     )
