43a9185d23d27f110ee34a5210280d91a64991ea
dopf129-pixel <dopf129@gmail.com>
2026-08-29 20:14:38 +0300

    Add period profit production factory

---
+++ app/period_profit_factory.py
@@ -0,0 +1,19 @@
+from config import TAX_RATE
+from services.cost_service import ProductCostService
+from services.finance_service import FinanceService
+from services.period_profit_query_service import PeriodProfitQueryService
+from services.period_profit_summary_service import PeriodProfitSummaryService
+from services.product_service import ProductService
+
+
+def create_period_profit_query():
+    product_service = ProductService()
+    summary_service = PeriodProfitSummaryService(
+        finance_service=FinanceService(),
+        cost_service=ProductCostService(),
+        tax_rate=TAX_RATE,
+    )
+    return PeriodProfitQueryService(
+        summary_service=summary_service,
+        product_provider=product_service.load_products,
+    )
