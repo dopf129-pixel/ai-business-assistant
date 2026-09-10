d422b168e836abe615568609915d7a3e09d849bd
dopf129-pixel <dopf129@gmail.com>
2026-09-10 09:46:30 +0300

    Tenant-isolate Return COGS authorization ledger

--- app/services/return_cogs_profit_application_authorization_repository.py
+++ app/services/return_cogs_profit_application_authorization_repository.py
@@ -2,6 +2,8 @@ from datetime import date, datetime
 from math import isfinite
 import sqlite3
 
+from services.tenant_storage import tenant_storage_path
+
 
 DB_NAME = "ozon_assistant.db"
 
@@ -19,7 +21,7 @@ class ReturnCogsProfitApplicationAuthorizationRepository:
         self.create_table()
 
     def get_connection(self):
-        return sqlite3.connect(DB_NAME)
+        return sqlite3.connect(tenant_storage_path(DB_NAME))
 
     def create_table(self):
         conn = self.get_connection()
